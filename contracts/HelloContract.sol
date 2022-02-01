60a93fb16222c198f2145d25589cc2d5bb35b551
nabil GHANMI <nabil.ghanmi@ausy.fr>
2022-02-01 23:57:59 +0100

    Hardhat Implementation on Testing Smart Contract With TypeScript

---
+++ contracts/HelloContract.sol
@@ -0,0 +1,9 @@
+//Code 4 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloContract{
+  function hello() public pure returns (string memory){
+    return "Hello, world from Web 3";
+  }
+}
